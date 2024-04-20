624304c7e014d741fffc3d92b0a56a5ed82acb92
salmannhr <salman.hamalarizky@gmail.com>
2024-04-20 07:55:25 +0700

    bebas

---
+++ modul8_1302220122/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace modul8_1302220122
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
